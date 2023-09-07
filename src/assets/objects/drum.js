3f0b3b2b4daca954f1ed9ef7e1c7510faeb0f16a
Carol A. Nikolic <carolnikolic@gmail.com>
2023-09-07 16:21:25 +0200

    feat: add main files

---
+++ src/assets/objects/drum.js
@@ -0,0 +1,25 @@
+const drum = [
+    {
+        imgSrc: "../public/images/tom1.png",
+    },
+    {
+        imgSrc: "../public/images/tom2.png",
+    },
+    {
+        imgSrc: "../public/images/tom3.png",
+    },
+    {
+        imgSrc: "../public/images/tom4.png",
+    },
+    {
+        imgSrc: "../public/images/snare.png",
+    },
+    {
+        imgSrc: "../public/images/crash.png",
+    },
+    {
+        imgSrc: "../public/images/kick.png",
+    },
+]
+
+export default drum
---
+++ src/assets/objects/drumSounds.js
@@ -0,0 +1,25 @@
+const drumSounds = [ 
+    {
+        soundSrc: "../public/sounds/tom-1.mp3",
+    },
+    {
+        soundSrc: "../public/sounds/tom-2.mp3",
+    },
+    {
+        soundSrc: "../public/sounds/tom-3.mp3",
+    },
+    {
+        soundSrc: "../public/sounds/tom-4.mp3",
+    },
+    {
+        soundSrc: "../public/sounds/snare.mp3",
+    },
+    {
+        soundSrc: "../public/sounds/crash.mp3",
+    },
+    {
+        soundSrc: "../public/sounds/kick-bass.mp3",
+    },
+]
+
+export default drumSounds
