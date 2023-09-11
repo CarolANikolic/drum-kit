25677497256868135f22dfa66a713d64e94635d8
Carol A. Nikolic <carolnikolic@gmail.com>
2023-09-11 21:56:12 +0200

    feat(home): add title and text to page

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+import Text from "./src/components/Text/index.js";
 import addBtnKey from "./src/assets/functions/addBtnKey.js";
 import drum from "./src/assets/objects/drum.js";
 import drumKey from "./src/assets/objects/drumKey.js";
 
+// Access and create main element's containers;
 const main = document.querySelector("main");
+const drumContainer = document.createElement("div");
 
-main.appendChild(addBtnKey(drum, drumKey));
+// Append text and drum buttons
+drumContainer.appendChild(addBtnKey(drum, drumKey));
+main.appendChild(Text("h1", "Drum Kit"));
+main.appendChild(drumContainer);
+main. appendChild(Text("p", "Made with ♥ by Carol"))
