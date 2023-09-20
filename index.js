29465159e1a7ac7c45c5d97c2c928657957c26fa
Carol A. Nikolic <carolnikolic@gmail.com>
2023-09-20 13:15:11 +0200

    feat(home): add styles on buttons/drum keys and text

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ import addBtnKey from "./src/assets/functions/addBtnKey.js";
 import drum from "./src/assets/objects/drum.js";
 import drumKey from "./src/assets/objects/drumKey.js";
 
-// Access and create main element's containers;
+// Access the main element's containers;
 const main = document.querySelector("main");
-const drumContainer = document.createElement("div");
 
 // Append text and drum buttons
-drumContainer.appendChild(addBtnKey(drum, drumKey));
 main.appendChild(Text("h1", "Drum Kit"));
-main.appendChild(drumContainer);
+main.appendChild(addBtnKey(drum, drumKey));
 main. appendChild(Text("p", "Made with ♥ by Carol"))
--- src/assets/functions/addBtnKey.js
+++ src/assets/functions/addBtnKey.js
@@ -1,6 +1,7 @@
 import Button from "../../components/Button/index.js";
 
 const instrumentContainer = document.createElement("div");
+instrumentContainer.classList.add("instrumentContainer");
 
 const addBtnKey = (instrument, instrumentKey) => {
     instrument.map((instrumentPart, index) => {
