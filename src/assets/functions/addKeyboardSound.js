9e0e06ea7dba4e77754d79c7a7df552d63fe1d73
Carol A. Nikolic <carolnikolic@gmail.com>
2023-10-03 21:07:26 +0200

    feat(home): make keyboard sound work if caps lock key is active

--- src/assets/functions/addKeyboardSound.js
+++ src/assets/functions/addKeyboardSound.js
@@ -2,7 +2,7 @@ const addKeyboardSound = (instrument, action) => {
     document.addEventListener("keydown", (e) => {
         for (let index = 0; index < instrument.length; index++) {
             let instrumentKey = instrument[index].instrumentKey
-            if (instrumentKey === e.key) {
+            if (instrumentKey === e.key.toLowerCase()) {
                 action(instrument, index);
             }  
         };
